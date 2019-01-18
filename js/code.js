c6ac7d045a3927f66aa120e217e9e58c87895b13
Omhet <omhetehmo@yandex.ru>
2019-01-18 12:14:28 +0300

    Load noise and sample sound

---
+++ js/code.js
@@ -0,0 +1,31 @@
+const slider = document.getElementById('slider');
+let noise;
+let sound;
+let soundPos;
+let playlists;
+let playlist;
+
+function preload() {
+    noise = loadSound('noise.mp3');
+    playlists = (JSON.parse(playlistsJSONString)).results;
+    playlist = (JSON.parse(samplePlaylistJSONString)).results[0].tracks;
+    sound = loadSound(playlist[0].audio);
+}
+
+function setup() {
+	createCanvas(innerWidth, innerHeight);
+	ellipseMode(RADIUS);
+	angleMode(DEGREES); 
+
+    noise.setLoop(true);
+    noise.setVolume(0);
+    // noise.play();
+	noise.setVolume(0.5, 5);
+
+	soundPos = floor(random(40, 100));
+}
+
+function draw() {
+	background(200);
+}
+
